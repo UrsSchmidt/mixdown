fae541337f2c44f1e255e0c44ed802e2f97320bc
UrsSchmidt <ursbjoernschmidt@googlemail.com>
2017-08-16 23:18:57 +0200

    Simplified examples.sh

--- examples.sh
+++ examples.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-if [ -d 'examples_wave' ]; then
-    rm examples_wave/*.wav
-    rmdir 'examples_wave'
-fi
-
 if [ ! -f 'mixdown' ]; then
     ./build.sh
 fi
 
 if [ -f 'mixdown' ]; then
+
     examplesdir='examples_wave'
+
     if [ -d "$examplesdir" ]; then
         rm $examplesdir/*.wav
     else
         mkdir "$examplesdir"
     fi
+
     for path in examples/*.mixdown; do
         filename=$(basename "$path" '.mixdown')
         ./mixdown -l '2' -w "$path" "$examplesdir/$filename.wav"
     done
+
 fi
