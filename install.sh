c38ef372e6ac2a21a7db58d9802224a7445ecb10
UrsSchmidt <ursbjoernschmidt@googlemail.com>
2017-09-10 12:51:00 +0200

    Removed sudo from scripts

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ fi
 
 if [ -f 'mixdown' ]; then
     if [ -d '/usr/local/bin/' ]; then
-        sudo cp 'mixdown' '/usr/local/bin/'
+        cp 'mixdown' '/usr/local/bin/'
     fi
 fi
 
 if [ -d '/usr/share/gtksourceview-3.0/language-specs/' ]; then
-    sudo cp 'mixdown.lang' '/usr/share/gtksourceview-3.0/language-specs/'
+    cp 'mixdown.lang' '/usr/share/gtksourceview-3.0/language-specs/'
 fi
 
 if [ -d '/usr/local/share/man/man1/' ]; then
-    sudo cp 'mixdown.1' '/usr/local/share/man/man1/'
-    sudo mandb -q
+    cp 'mixdown.1' '/usr/local/share/man/man1/'
+    mandb -q
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -f '/usr/local/bin/mixdown' ]; then
-    sudo rm '/usr/local/bin/mixdown'
+    rm '/usr/local/bin/mixdown'
 fi
 
 if [ -f '/usr/share/gtksourceview-3.0/language-specs/mixdown.lang' ]; then
-    sudo rm '/usr/share/gtksourceview-3.0/language-specs/mixdown.lang'
+    rm '/usr/share/gtksourceview-3.0/language-specs/mixdown.lang'
 fi
 
 if [ -f '/usr/local/share/man/man1/mixdown.1' ]; then
-    sudo rm '/usr/local/share/man/man1/mixdown.1'
-    sudo mandb -q
+    rm '/usr/local/share/man/man1/mixdown.1'
+    mandb -q
 fi
