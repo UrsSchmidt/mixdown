fecc93feacce0f04997d9aa1bd1edfb9bc8046d9
Urs-Björn Schmidt <ursbjoernschmidt@gmail.com>
2024-07-31 17:13:37 +0200

    Moved to c18.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 
 flex -o 'src/lex.yy.c' 'src/mixdown.l'
 bison -d -o 'src/y.tab.c' 'src/mixdown.y'
-gcc -std=c11 -Wall -Wpedantic -o 'mixdown' src/*.c -lm
+gcc -std=c18 -Wall -Wpedantic -o 'mixdown' src/*.c -lm
